c96aff1f6081696da943c3574934acbb8bb8f7d3
Valerialope <137837760+Valerialope@users.noreply.github.com>
2025-05-01 15:11:54 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ColaHeroes cola = new ColaHeroes();
+        cola.encolarHeroes(); // R1
+        cola.mostrarPoderesTotales(); // R2
+    }
+}
